1ff64d7fcf8c9e4a9a133ddb60d850c8bbd688b8
shailesh <shaileshbind39@gmail.com>
2025-07-09 16:57:58 +0530

    initial commit

---
+++ file.js
@@ -0,0 +1,2 @@
+const fs = require("fs");
+fs.writeFileSync("./text.txt", "Hello, World!");
---
+++ hello.js
@@ -0,0 +1,3 @@
+const { add, sub } = require("./math");
+console.log(add(2, 8)); // This will log 5 to the console
+console.log(sub(8, 5)); // This will log 5 to the console

9f429ba06e54280bb15965fdcc76af98498f906b
shailesh <shaileshbind39@gmail.com>
2025-07-10 15:34:08 +0530

    switch case in node

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
 const http = require("http");
+const fs = require("fs");
 
 const myServer = http.createServer((req, res) => {
-  console.log("Request received:", req.headers);
-  res.end("Hello From Server!!");
+  const logData = `Request received at: ${new Date().toISOString()} on ${
+    req.url
+  }\n`;
+
+  fs.appendFile("log.txt", logData, (err, data) => {
+    switch (req.url) {
+      case "/":
+        res.end("Home page!");
+        break;
+      case "/contact":
+        res.end("Contact page!");
+        break;
+      case "/about":
+        res.end("About page!");
+        break;
+      default:
+        res.end("404 Not Found!");
+    }
+    if (err) {
+      console.error("Error writing to log file:", err);
+    }
+  });
 });
 
 const port = 5000;
